687c4204e174673f15e87764a1f8db5d53d0aec5
masaya0925 <masaya_suda0925@i.softbank.jp>
2016-10-26 21:16:12 +0900

    add ProjectFile

---
+++ Mezamasi/Mezamasi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mezamasi
+//
+//  Created by Ma-Kun on 2016/10/26.
+//  Copyright © 2016年 Ma-Kun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Mezamasi/Mezamasi/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Mezamasi
+//
+//  Created by Ma-Kun on 2016/10/26.
+//  Copyright © 2016年 Ma-Kun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
