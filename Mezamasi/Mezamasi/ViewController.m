731ca5f8b7251b0beea9c0dbf8c5ebee46abd5a3
masaya0925 <masaya_suda0925@i.softbank.jp>
2016-10-27 11:04:15 +0900

    現在時刻を取得

--- Mezamasi/Mezamasi/ViewController.m
+++ Mezamasi/Mezamasi/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSDate *date = [NSDate date];
+    NSLog(@"%@",date);
+    
 }
 
 
